a90fed5a9f1e7f175b4e5f3e467f1de5d7fc30b5
tushar27 <tushar.27041994@gmail.com>
2024-05-22 23:16:15 +0530

    buttercup chat

--- buttercup/chat/views.py
+++ buttercup/chat/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import ChatRoom, ChatMessage
+
+def index(request):
+    rooms = ChatRoom.objects.all()
+    return render(request, 'chat/index.html', {'rooms': rooms})
+
+def room(request, room_name):
+    room, created = ChatRoom.objects.get_or_create(name=room_name)
+    messages = ChatMessage.objects.filter(room=room)
+    return render(request, 'chat/room.html', {'room': room, 'messages': messages})
