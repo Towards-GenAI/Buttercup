207dcb15de81ee012b094bdc01f805b788f9d142
tushar27 <tushar.27041994@gmail.com>
2024-05-22 23:08:33 +0530

    settings

--- buttercup/buttercup/settings.py
+++ buttercup/buttercup/settings.py
@@ -9,7 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
